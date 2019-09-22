758b6f40790eff66aea89be0eb94f38b88c189e2
Ivan Olin <olinid@utexas.edu>
2019-09-22 09:25:07 -0500

    start reimplement cutscenes

---
+++ Assets/PhysChunk.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysChunk : MonoBehaviour
+{
+    public Vector3 Destination;
+    public float EstimatedFallTime;
+
+    void OnCollisionEnter2D(Collision2D other) {
+        if(other.gameObject.tag.Equals("CollectibleBone"))
+        {
+            StartCoroutine(Fall());
+        }
+    }
+
+    public IEnumerator Fall()
+    {
+        GetComponent<Rigidbody2D>().freezeRotation = false;
+        GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None;
+        yield return new WaitForSeconds(EstimatedFallTime);
+        transform.position = Destination;
+    }
+}
---
+++ Assets/Scripts/EnemyDeath.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDeath : MonoBehaviour
+{
+    public GameObject CollectiblePrefab;
+    public int MinDrop;
+    public int MaxDrop;
+    public float DropForceMagnitude;
+    public int HP = 2;
+
+    public void Damage(int amount)
+    {
+        HP -= amount;
+        if (HP <= 0)
+            Die();
+    }
+
+    public void Die()
+    {
+
+        int amt = Random.Range(MinDrop, MaxDrop);
+        for (int i = 0; i < amt; i++)
+        {
+            GameObject g = GameObject.Instantiate(CollectiblePrefab);
+            g.GetComponent<CollectibleBone>().CanCollect = true;
+            g.transform.position = transform.position;
+            g.GetComponent<Rigidbody2D>().AddForce(new Vector3(
+                Random.Range(-1, 1),
+                Random.Range(-1, 1),
+                0
+                ).normalized * DropForceMagnitude);
+        }
+
+        GameObject.Destroy(this.gameObject);
+    }
+}
