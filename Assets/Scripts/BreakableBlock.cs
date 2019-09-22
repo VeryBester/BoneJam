001a1cbda12e9490af44687dbaa04fca8543a986
verybester <david15202@gmail.com>
2019-09-22 02:40:52 -0500

    Made the falling blocks

---
+++ Assets/Scripts/BreakableBlock.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakableBlock : MonoBehaviour
+{
+
+    /* 
+    To Dane: 
+        
+    Tag the bones as bones 
+    
+    Love, 
+    David
+    */
+    private void OnCollisionEnter2D(Collision2D other) {
+        if(other.gameObject.CompareTag("Bone")){
+            Destroy(this);
+        }
+    }
+}
